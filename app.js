1b06db97faeca1a9548ee093e8f3f102c5058173
BorovlevAS <bas@qdes.com.ua>
2022-03-19 19:54:24 +0200

    initial git

---
+++ app.js
@@ -0,0 +1,23 @@
+const tPost = {
+	author: 'Artem',
+	defs: {
+		main1: 'main1',
+		main2: 'main2'
+	}
+};
+
+const fnAddPost = (tPost, sMessage, dateAdd = Date()) => {
+	const newPost = {
+		...tPost,
+		message: sMessage,
+		dateAdd
+	}
+	
+	newPost.defs.main1 = 'Here';
+	
+	return newPost;
+}
+const aPost = fnAddPost(tPost, 'Hi there!');
+
+console.log(aPost);
+console.log(tPost);
